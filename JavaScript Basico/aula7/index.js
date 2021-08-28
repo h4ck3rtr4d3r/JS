9faae9f22c0b8d790ba63635467d8cd2e6b297bc
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-08-28 12:43:32 -0400

    update

---
+++ JavaScript Basico/aula7/index.js
@@ -0,0 +1,13 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode comecar o nome de uma constante com um número
+// Não pode conter espacos ou tracos
+// Utilizamos camelCase
+// Case-sensitive
+// Nao podemos redeclarar variaveis com let
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+
+console.log(resultado);
