17783e69355dc9ee25578ada2f924a8c361ab5b8
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-08-28 11:46:36 -0400

    upgrade

---
+++ JavaScript Basico/aula6/index.js
@@ -0,0 +1,16 @@
+let nome = 'Felipe';
+let esposa = 'Tabata'
+let filho = 'Dominc'
+let filha = 'Ariella'
+console.log(nome,'nasceu em 1982')
+console.log('Em 2016', nome, 'conheceu',esposa)
+console.log(nome,' casou-se com', esposa, 'em 2016')
+console.log('O filho de', nome, 'se chama', filho, 'e a filha de', nome, 'se chama', filha)
+
+// Nao podemos criar variaveis com palavras reservadas
+// Variaveis precisam ter nomes significativos
+// Nao pode comecar o nome de uma variavel com um numero
+// Nao podemos conter espacos ou tracos
+// Utilizamos camelCase
+// JavaScript e case-sensitive
+// Nao podemos redeclarar variaveis com let
