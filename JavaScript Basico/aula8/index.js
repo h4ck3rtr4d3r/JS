09172666a57c46f04d82d8d732371031abbad92f
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-08-29 07:19:56 -0400

    update

--- JavaScript Basico/aula8/index.js
+++ JavaScript Basico/aula8/index.js
@@ -2,8 +2,8 @@
 Felipe Oliveira Silva tem 39 anos, pesa 72kg tem 1.70 de altura e seu IMC é de 17.42
 */
 
-const nome = 'Felipe';
-const sobrenome = 'de Oliveira Silva';
+const nome = 'H4ck3r';
+const sobrenome = 'Tr4d3r';
 let idade = 39;
 let peso = 70;
 let altura = 1.74;
