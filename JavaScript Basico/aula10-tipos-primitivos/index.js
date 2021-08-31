07bed02eaae6b605bd7269914322eaa8a5a51860
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-08-31 07:01:51 -0400

    update

---
+++ JavaScript Basico/aula10-tipos-primitivos/index.js
@@ -0,0 +1,10 @@
+// O tipos primitivos são: string, number, undefined, null, boolean
+
+const nome = 'H4ck3r';                  // string
+const nome1 = "H4ck3r";                 // string
+const nome3 = `H4ck34`;                 // string
+const num1 = 10;                        // number
+const num2 = 10.52;                     // numer
+let nomeAluno;                          // undefined => não aponta para local nenhum na memória
+const sobrenomeAluno = null;            // Nulo => não aponta para local nenhum na memória
+const aprovado = false;                 // Boolean = true, false (lógico)
