8f433d9a2fdfb3f575285d91376b12951f5fcf70
h4ck3rtr4d3r <h4ck3rtr4d3r@gmail.com>
2021-09-01 09:02:04 -0400

    update

---
+++ JavaScript Basico/aula10.1-operadores-aritmeticos-atribuicao-incremento/index.js
@@ -0,0 +1,31 @@
+/* Operadores Aritméticos 
+
++ (soma e faz concatenação quando o número está entre aspas simples, ou seja, uma string), 
+- (subtração)
+/ (Divisão)
+* (Multiplicação)
+**(Potenciação)
+% (Modulo / Resto)
+
+Precedencias
+1 -> Parentese
+2 -> **
+3 -> *, /, %
+4 -> + , -
+
+*/
+
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+
+// Incremento e Decremento
+
+let contadorIncremento = 1;
+contadorIncremento++;
+console.log(contadorIncremento);
+
+// Operdadores de atribuição
+let contadorAtrib = 2;
+contadorAtrib **= 10;
+console.log(contadorAtrib);
